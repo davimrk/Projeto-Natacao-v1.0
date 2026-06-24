9feb0ea59a5b1640f89864045343d756b5865727
Davi Mark Souza Franco <1115226@senacminas.edu.br>
2026-06-24 16:44:51 -0300

    v1.2.1

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,8 @@
-const API = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://natacao-backend.onrender.com";
+const API =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:3000"
+    : "https://natamentos.onrender.com";
 
 async function cadastrarAluno(event) {
   event.preventDefault();
